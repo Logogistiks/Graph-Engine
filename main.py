d90e0444b704594b50d090a23f302ca57e3a6f13
Jan <jan@seifert-online.de>
2023-06-24 23:55:12 +0200

    added picture

--- main.py
+++ main.py
@@ -23,4 +23,19 @@ obj5.register_child(obj5)
 
 mygraph = get_graph(obj0)"""
 
-draw(generate_graph(10, 6))
+#draw(generate_graph(10, 6))
+
+node0 = Node(value=0)
+node1 = Node(value=1)
+node2 = Node(value=2)
+
+node3 = Node(value=3, parents=[node0])
+node1.register_parent(node0)
+node1.register_child(node2)
+
+node1.print_parents()
+node1.print_childs()
+
+my_graph = get_graph(node0)
+
+draw(my_graph)
